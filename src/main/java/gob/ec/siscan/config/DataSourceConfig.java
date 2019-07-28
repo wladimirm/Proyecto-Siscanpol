a7070b8064163b926e39ec5c5a6d1e20ec289844
Kleber Mendoza <kleber@LKM-Q-MENDOZAKL.mineria.local>
2019-07-28 05:22:47 -0500

    conf heroku

--- src/main/java/gob/ec/siscan/config/DataSourceConfig.java
+++ src/main/java/gob/ec/siscan/config/DataSourceConfig.java
@@ -1,25 +1,25 @@
 package gob.ec.siscan.config;
 
-import javax.sql.DataSource;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-@Profile("prod")
 @Configuration
-public class DatabaseConfig {
-	 @Value("${spring.datasource.url}")
+public class DataSourceConfig {
+	
+	  @Value("${spring.datasource.url}")
 	  private String dbUrl;
-
+	  @Profile("prod")
 	  @Bean
 	  public DataSource dataSource() {
 	      HikariConfig config = new HikariConfig();
 	      config.setJdbcUrl(dbUrl);
-	      return new HikariDataSource(config);
+	      return (DataSource) new HikariDataSource(config);
 	  }
-
 }
